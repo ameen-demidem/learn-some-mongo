3d7db1935f5a2dbdc404746b752eff24a196a2a9
Ameen Demidem <ameen.demidem@gmail.com>
2016-12-23 18:51:47 -0500

    Add solution to exercise 3

---
+++ 03.js
@@ -0,0 +1,25 @@
+const mongodb = require('mongodb');
+
+const url = 'mongodb://localhost:27017/learnyoumongo';
+const ageThreshold = Number(process.argv[2]) || 0;
+
+const mongo = mongodb.MongoClient;
+mongo.connect(url, function(err, db) {
+  if (err) {
+    console.error('Something went wrong: ', err); 
+  } else {
+    db
+      .collection('parrots')
+      .find({
+        age: { $gt: ageThreshold }
+      })
+      .toArray((err, parrots) => {
+        if (err) {
+          console.error('Something went wrong: ', err); 
+        } else {
+          console.log(parrots);
+        }
+      });
+    db.close();
+  }
+});
